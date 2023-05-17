ed38a5b13d87eac5219c34f5284da76bf4269f29
DashaArtemieva <132048705+DashaArtemieva@users.noreply.github.com>
2023-05-17 12:28:02 +0300

    Delete Gen_and_send_key_server.sh

--- Gen_and_send_key_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Генерация RSAKey
-ssh-keygen
-
-# Отправка ключа на удаленный хост
-sh ssh-copy-id admin1@192.168.225.134 # User@remote_host
-
-echo "Открытый ключ отправлен на удаленный хост"
